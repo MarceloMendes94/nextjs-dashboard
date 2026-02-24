7e98bfdc8ad0084ed5b3eaed3c1fbc531f9ef284
marceloGlobalsys_notebook <marcelocelularxiaomi@gmail.com>
2026-02-24 09:04:34 -0300

    capitulo 9 streaming
    streaming da pagina completa skeleton

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -7,7 +7,10 @@ import { fetchRevenue, fetchLatestInvoices,fetchCardData } from '@/app/lib/data'
 export default async function Page() {
   const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
-  const {totalPaidInvoices,totalPendingInvoices,numberOfInvoices,numberOfCustomers} = await fetchCardData();
+  const { totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
